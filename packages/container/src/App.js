8939c10033a14c581e45164517269692fe498ca7
Nityanand Rai <nityanand_rai@epam.com>
2025-06-05 22:48:38 +0530

    added header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import MarketingRoot from './components/MarketingRoot';
+import MarketingRoot from "./components/MarketingRoot";
+import Header from "./components/Header";
 
 const App = () => {
-    return <>
-    <h1>Marketing Page</h1>
-    <hr/>
-       <MarketingRoot/>
-    </>
-}
-
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingRoot />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
